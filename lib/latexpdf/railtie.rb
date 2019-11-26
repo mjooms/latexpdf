9990d5f6566886ff0728e5226c9e426e7d3ef0c1
Matthijs Ooms <matthijs-github@freemo.nl>
2019-11-26 14:00:42 +0100

    Register :tex Mime type

--- lib/latexpdf/railtie.rb
+++ lib/latexpdf/railtie.rb
@@ -3,6 +3,12 @@ require "action_view/template_handlers/tex"
 module Latexpdf
   class Railtie < Rails::Railtie
     config.to_prepare do
+      begin
+        Mime::Type.lookup(:tex)
+      rescue Mime::Type::InvalidMimeType
+        Mime::Type.register('application/x-tex', :tex)
+      end
+
       ActionView::Template.register_template_handler :tex, ActionView::Template::Handlers::Tex
       Latexpdf.configure do |c|
         c.build_path = Rails.configuration.paths['tmp'].first
